ebb6fa7865a4cf59f25697dc96a1e1d98a7b2600
praga <praga@SKPDON-2004>
2024-11-04 14:57:47 +0530

    hi

---
+++ C2TC_PRAGADEESWAR/src/com/tnsif/daythree/EncapsulationDemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.daythree;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OopsConceptDemo a=new OopsConceptDemo();
+		a.setAge(12);
+		a.setName("praga");
+		a.setSerialNum(123);
+		System.out.println(a);
+
+	}
+
+}
