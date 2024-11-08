c0fe726dcd94d20eef26e968b0c9a46516efb531
praga <praga@SKPDON-2004>
2024-11-08 12:51:21 +0530

    hi

---
+++ C2TC_PRAGADEESWAR/src/com/tnsif/dayfour/STATICVARIABLE/FinalMethodClassTest.java
@@ -0,0 +1,6 @@
+package com.tnsif.dayfour.STATICVARIABLE;
+public class FinalMethodClassTest extends FinalMethodClass  {
+		  @Override void show() {
+		 System.out.println("This is the final method of FinalMethodEx class"); }
+
+	}
